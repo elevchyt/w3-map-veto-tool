7ee7fb37b121c71e6756a1bb20622e39cc3f3b4a
elevchyt <lehy64@gmail.com>
2024-10-18 19:19:06 +0300

    spinner & favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import Spinner from "@/components/Spinner";
+
 export default function Home() {
   return (
     <>
-      <h1>home</h1>
+      <h1>Map Veto Tool</h1>
+      <Spinner />
     </>
   );
 }
