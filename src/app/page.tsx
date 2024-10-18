715de8d00e06e4fdf640f2a206baf3734d060585
elevchyt <lehy64@gmail.com>
2024-10-18 21:07:14 +0300

    styles updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import {
-  isPendingAtom,
-  mapPoolsAtom,
-  maps1v1Atom,
-  maps2v2Atom,
-  maps3v3Atom,
-  maps4v4Atom,
-  mapsAllTheRandoms1v1Atom,
-} from "@/atoms/atoms";
+import { isPendingAtom, mapPoolsAtom } from "@/atoms/atoms";
 import ErrorHint from "@/components/ErrorHint";
 import Loading from "@/components/Loading";
 import { useAtom } from "jotai";
@@ -17,13 +9,6 @@ import { useEffect } from "react";
 
 export default function Home() {
   const [isPending, setIsPending] = useAtom(isPendingAtom);
-  const [maps1v1, setMaps1v1] = useAtom(maps1v1Atom);
-  const [maps2v2, setMaps2v2] = useAtom(maps2v2Atom);
-  const [maps3v3, setMaps3v3] = useAtom(maps3v3Atom);
-  const [maps4v4, setMaps4v4] = useAtom(maps4v4Atom);
-  const [mapsAllTheRandoms1v1, setMapsAllTheRandoms1v1] = useAtom(
-    mapsAllTheRandoms1v1Atom
-  );
   const [mapPools, setMapPools] = useAtom(mapPoolsAtom);
 
   const w3championsLadderDataURL =
@@ -35,18 +20,12 @@ export default function Home() {
       .then((res) => res.json())
       .then((data) => {
         const mapsPerGameMode = _.groupBy(data, "name");
-        console.log(mapsPerGameMode);
         const maps1v1 = mapsPerGameMode["1 vs 1"][0]["maps"];
         const maps2v2 = mapsPerGameMode["2 vs 2"][0]["maps"];
         const maps3v3 = mapsPerGameMode["3 vs 3"][0]["maps"];
         const maps4v4 = mapsPerGameMode["4 vs 4"][0]["maps"];
         const mapsAllTheRandoms1v1 =
           mapsPerGameMode["All The Randoms 1vs1"][0]["maps"];
-        setMaps1v1(maps1v1);
-        setMaps2v2(maps2v2);
-        setMaps3v3(maps3v3);
-        setMaps4v4(maps4v4);
-        setMapsAllTheRandoms1v1(mapsAllTheRandoms1v1);
 
         const newMapPools = [
           {
@@ -79,23 +58,19 @@ export default function Home() {
       .finally(() => {
         setIsPending(false);
       });
-  }, [
-    setIsPending,
-    setMapPools,
-    setMaps1v1,
-    setMaps2v2,
-    setMaps3v3,
-    setMaps4v4,
-    setMapsAllTheRandoms1v1,
-  ]);
+  }, [setIsPending, setMapPools]);
+
+  const launchLobby = () => {
+    console.log("done");
+  };
 
   return (
     <>
       <h1>Map Veto Tool</h1>
 
       {/* Map Pool Selection */}
-      <p>Choose a map pool.</p>
-      {maps1v1.length && !isPending ? (
+      <p>Choose a map pool:</p>
+      {mapPools.length && !isPending ? (
         <select>
           {mapPools.map((map: unknown) => (
             <option key={map.id} value={map.name}>
@@ -114,10 +89,18 @@ export default function Home() {
       </div>
 
       {/* Action Buttons */}
-      <button>Launch Lobby</button>
+      <button
+        className="generic-button"
+        onClick={() => {
+          launchLobby();
+        }}
+        disabled={isPending || !mapPools.length}
+      >
+        Launch Lobby
+      </button>
 
       {/* Feedback */}
-      {!maps1v1.length && !isPending ? <ErrorHint /> : null}
+      {!mapPools.length && !isPending ? <ErrorHint /> : null}
     </>
   );
 }
