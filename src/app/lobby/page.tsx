16be565fbf7f9e5aedfd2631217931aac7f0ef73
elevchyt <lehy64@gmail.com>
2024-10-18 23:37:57 +0300

    firebase setup

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import { get, ref } from "firebase/database";
+import { database } from "@/firebase/firebase";
+import { useEffect } from "react";
+
 export default function Lobby() {
+  useEffect(() => {
+    const lobbiesRef = ref(database, "lobbies/1");
+    get(lobbiesRef).then((snapshot) => {
+      console.log(snapshot.val());
+    });
+  }, []);
+
   return (
     <>
       <h1>lobby</h1>
---
+++ src/firebase/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "w3-map-veto-tool-b3c87.firebaseapp.com",
+  databaseURL:
+    "https://w3-map-veto-tool-b3c87-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "w3-map-veto-tool-b3c87",
+  storageBucket: "w3-map-veto-tool-b3c87.appspot.com",
+  messagingSenderId: "314245453265",
+  appId: "1:314245453265:web:ba58063c732a8ac22bfcf0",
+  measurementId: "G-XZXJBTP2CW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const database = getDatabase();
