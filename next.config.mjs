bac5bfb68fdc1ff2dc964b57fb1896a1ead10cf2
elevchyt <lehy64@gmail.com>
2024-10-20 00:54:20 +0300

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: ["d3upx5peno0o6w.cloudfront.net"], // warcraft3.info API endpoint
   },
