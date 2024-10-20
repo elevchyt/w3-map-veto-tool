b651155f0474a2a4fdcb9cb69b03b2dba51e3f90
elevchyt <lehy64@gmail.com>
2024-10-20 23:41:35 +0300

    lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ import NavLinks from "@/components/NavLinks";
 
 export const metadata: Metadata = {
   title: "W3 Map Veto Tool",
-  description: "Streamlined ban/pick map process for Warcraft 3 leagues and tournaments.",
+  description:
+    "Streamlined ban/pick map process for Warcraft 3 leagues and tournaments.",
 };
 
 export default function RootLayout({
